c4166c38ba58a9a7353dc423e31b8361db535f04
Maynor Lopez <maynor94l39@gmail.com>
2026-07-28 20:51:03 -0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import profileImage from '../public/perfil.webp'
+
+function App() {
+
+  return (
+    <img className='w-100' src={profileImage} alt="" />
+  )
+}
+
+export default App
