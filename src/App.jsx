8993fc4fdf254cddc8fa241e0e112f3e09da29c3
Maynor Lopez <maynor94l39@gmail.com>
2026-08-11 21:02:17 -0600

    Styling navbar and make it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
-import profileImage from '../public/perfil.webp'
+import profileImage from '../public/profile.png'
 import Header from './components/Header'
 
 function App() {
 
   return (
-    <div className='w-full min-h-screen bg-slate-800'>
+    <div className='w-full min-h-screen flex flex-col items-center font-Inter bg-gradient-to-b from-slate-950 to-teal-950'>
       <Header/>
-      <img src={profileImage} alt="" />
+      <img className='w-100' src={profileImage} alt="" />
     </div>
   )
 }
